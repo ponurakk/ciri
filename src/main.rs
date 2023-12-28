0fde6d12555b05729843a734b7e82a17c952e4c9
ponurakk <p0unrakg@int.pl>
2023-12-28 19:04:43 +0100

    Reset window when panicking

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ mod components;
 mod parsers;
 mod validators;
 
-use std::io::Write;
+use std::io::{self, Write};
 
 use ciri::args::{PackageSubCommands, ProjectSubCommands};
 use ciri::Cli;
 use clap::Parser;
+use crossterm::terminal::{disable_raw_mode, LeaveAlternateScreen};
 use flexi_logger::DeferredNow;
 use log::Record;
+use miette::IntoDiagnostic;
 use nu_ansi_term::Color;
 
 use self::commands::package;
@@ -41,6 +43,16 @@ fn my_format(
 }
 
 fn main() -> miette::Result<()> {
+    let original_hook = std::panic::take_hook();
+
+    std::panic::set_hook(Box::new(move |panic| {
+        disable_raw_mode().into_diagnostic().unwrap();
+        crossterm::execute!(io::stdout(), LeaveAlternateScreen)
+            .into_diagnostic()
+            .unwrap();
+        original_hook(panic);
+    }));
+
     let logger_config = "info";
     flexi_logger::Logger::try_with_str(logger_config)
         .expect("Logger config string formatted incorrectly")
